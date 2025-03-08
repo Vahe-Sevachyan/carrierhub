4e1094ebd979e8f0bea8fcbd6ee5156c4af6dbec
Vahe Sevachyan <vscode13@gmail.com>
2025-03-08 14:32:33 -0800

    added EmailList component

---
+++ src/components/EmailList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="email-list">
+    <h2>{{ region }}</h2>
+    <button @click="$emit('add-client')">Create New Email Client</button>
+    <ul>
+      <li v-for="client in clients" :key="client.id">
+        {{ client.name }} - {{ client.email }}
+        <button @click="$emit('edit-client', client)">Edit</button>
+        <button @click="$emit('delete-client', client.id)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+defineProps(["region", "clients"]);
+</script>
